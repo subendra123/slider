5366cb334809ecf4d80f2b20c59af1b994c45b22
subendra <subendra483@gmail.com>
2024-08-10 23:09:57 +0545

    slider starting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { product } from "./components/Data";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import Slider from "react-slick";
 
 function App() {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="carousel">
+        <h2>Carousel Tutorial</h2>
+
+        <Slider  {...settings}>
+
+          {
+            product.map((item) => {
+              const{id, image, title, price, description} = item
+              return(
+                <>
+                
+                <div className="card">
+                  <div className="image-box">
+                  <img src="images/mac.jpeg" alt="" />
+                  </div>
+
+                  <div className="product_info">
+                    <h3>{title}</h3>
+                    <p>This is product Description here</p>
+                    <h3>$ {price}</h3>
+                  </div>
+                  <div className="button_section">
+                    <button>Add TO Cart</button>
+        </div>
+      </div>
+                
+                </>
+              )
+
+            })
+          }
+    
+
+
+      {/* <div className="box">
+        <h3>2</h3>
+      </div>
+      <div className="box">
+        <h3>3</h3>
+      </div>
+      <div className="box">
+        <h3>4</h3>
+      </div>
+      <div className="box">
+        <h3>5</h3>
+      </div>
+      <div className="box">
+        <h3>6</h3>
+      </div>
+      <div className="box">
+        <h3>7</h3>
+      </div>
+      <div className="box">
+        <h3>8</h3>
+      </div> */}
+
+      </Slider>
+
+
+      </div>
+
+ 
+    </>
   );
 }
 
